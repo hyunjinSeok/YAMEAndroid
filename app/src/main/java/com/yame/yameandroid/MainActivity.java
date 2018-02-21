452f709389ec61e2c9bb31126ea7415db6918db9
hyunjinSeok <tjrguswls123@naver.com>
2018-02-22 06:51:47 +0900

    test

--- app/src/main/java/com/yame/yameandroid/MainActivity.java
+++ app/src/main/java/com/yame/yameandroid/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //이것은 무엇인가?
     }
 }
